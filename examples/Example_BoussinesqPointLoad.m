ca5a5f01cafdc0abd5be9ba4fb54572fb26d0125
keshavsrao <128393037+keshavsrao@users.noreply.github.com>
2026-07-03 15:23:53 +0530

    BoussinesqPointLoad

--- examples/Example_BoussinesqPointLoad.m
+++ examples/Example_BoussinesqPointLoad.m
@@ -3,12 +3,11 @@ clc;
 
 run('../scripts/SetupGeoFoundation.m')
 
-P = 1000;      % kN
-
-r = 1.0;       % m
-
-z = 2.0;       % m
+P = 1000;     % kN
+r = 1.5;      % m
+z = 3.0;      % m
 
 sigma = gf_BoussinesqPointLoad(P,r,z);
 
-fprintf('Vertical Stress = %.3f kPa\n',sigma);
+fprintf('\n');
+fprintf('Vertical Stress = %.4f kPa\n',sigma);
--- src/stress/gf_BoussinesqPointLoad.m
+++ src/stress/gf_BoussinesqPointLoad.m
@@ -6,7 +6,15 @@
 %   gf_BoussinesqPointLoad
 %
 % Description:
-%   Calculates vertical stress due to a surface point load.
+%   Computes the vertical stress due to a surface point load.
+%
+% Inputs:
+%   P - Point load (kN)
+%   r - Radial distance from load (m)
+%   z - Depth below ground surface (m)
+%
+% Output:
+%   sigma_z - Vertical stress (kPa)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -14,6 +22,6 @@ function sigma_z = gf_BoussinesqPointLoad(P,r,z)
 
 R = sqrt(r^2 + z^2);
 
-sigma_z = (3*P*z^3) / (2*pi*R^5);
+sigma_z = (3*P*z^3)/(2*pi*R^5);
 
 end
